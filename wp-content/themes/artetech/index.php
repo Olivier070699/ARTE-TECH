c405e2b9d262bb8ae892f3d7ec3829196a1c5089
Olivier Decock <olivdeco1@arteveldehs.be>
2019-12-17 22:49:17 +0100

    home & contact update

--- wp-content/themes/artetech/index.php
+++ wp-content/themes/artetech/index.php
@@ -4,12 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo the_title(); ?></title>
+    <title>ARTE-TECH | <?php echo the_title(); ?></title>
     <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
 </head>
 <body>
-    <div class="sidebar"></div>
-    <div class="container"></div>
+    <div class="sidebar">
+       <?php wp_nav_menu() ?>
+    </div>
+    <div class="container">
+        <?php
+                if ( have_posts() ) 
+                : while ( have_posts() ) : the_post();
+                    the_content();
+                endwhile; 
+            endif;
+        ?>
+    </div>
     <?php wp_footer(); ?>
 </body>
 </html>
